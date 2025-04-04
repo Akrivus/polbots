c03d74757a5e3a46e1004adca3abc3472f72b3c6
Owen Adrian <akrivus@gmail.com>
2025-04-04 12:46:08 -0700

    dog food

--- Assets/Scripts/Integrations/Config/DiscordConfigs.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-public class DiscordConfigs : IConfig
-{
-    public string Type => "discord";
-    public Dictionary<string, string> WebhookURLs { get; set; }
-}
--- Assets/Scripts/Integrations/Config/SplashScreenConfigs.cs
+++
@@ -1,7 +0,0 @@
-﻿public class SplashScreenConfigs : IConfig
-{
-    public string Type => "splash";
-    public string[] Splashes { get; set; }
-    public float TitleDuration { get; set; }
-    public float SplashDuration { get; set; }
-}
