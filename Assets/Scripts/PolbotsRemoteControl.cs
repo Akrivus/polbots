b6542a97bb86b8594ec6468abbd71f9fab5ade75
Owen Adrian <akrivus@gmail.com>
2025-04-22 09:51:11 -0700

    Temporary workaround for RC integration (duplicate PolbotsRemoteControl)

--- Assets/Scripts/PolbotsRemoteControl.cs
+++ Assets/Scripts/PolbotsRemoteControl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
 
-public class RemoteControl : MonoBehaviour
+public class PolbotsRemoteControl : MonoBehaviour
 {
     private Dictionary<string, Action> buttonActions;
 
