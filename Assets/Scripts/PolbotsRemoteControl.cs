2644ebb1a07b13cc6bd96a9a576e907792d669e2
Owen Adrian <akrivus@gmail.com>
2025-04-22 15:38:33 -0700

    Custom RC implementation.

--- Assets/Scripts/PolbotsRemoteControl.cs
+++ Assets/Scripts/PolbotsRemoteControl.cs
@@ -4,74 +4,20 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
 
-public class PolbotsRemoteControl : MonoBehaviour
+public class PolbotsRemoteControl : RemoteControl
 {
-    private Dictionary<string, Action> buttonActions;
-
-    private void OnEnable()
-    {
-        buttonActions = new Dictionary<string, Action>
-        {
-            { "leftButton", Pause },
-            { "leftArrow", Skip },
-            { "rightArrow", Replay },
-            { "upArrow", SpeedUp },
-            { "downArrow", SlowDown },
-            { "pageUp", SportsVolumeUp },
-            { "pageDown", SportsVolumeDown },
-            { "rightButton", Drop },
-            { "contextMenu", ToggleGame }
-        };
-    }
-
-    private void Awake()
-    {
-        InputSystem.onAnyButtonPress.Call((button) => buttonActions.GetValueOrDefault(button.name)?.Invoke());
-    }
-
-    private void Pause()
-    {
-        ChatManager.IsPaused = !ChatManager.IsPaused;
-    }
-
-    private void Skip()
-    {
-        ChatManager.SkipToEnd = true;
-    }
-
-    private void Replay()
-    {
-        var chat = ChatManager.Instance.NowPlaying;
-        ChatManager.Instance.AddToPlayList(chat);
-    }
-
-    private void SpeedUp()
-    {
-        ActorController.GlobalSpeakingRate -= 0.1f;
-    }
-
-    private void SlowDown()
-    {
-        ActorController.GlobalSpeakingRate += 0.1f;
-    }
-
-    private void SportsVolumeUp()
-    {
-        SoccerGameSource.Instance.IncrementVolume();
-    }
-
-    private void SportsVolumeDown()
-    {
-        SoccerGameSource.Instance.DecrementVolume();
-    }
-
-    private void Drop()
+    public override void DownArrow()
     {
         RedditSource.Instance.TriggerDrop();
     }
 
-    private void ToggleGame()
+    public override void UpArrow()
     {
         SoccerGameSource.Instance.ToggleGame();
+    }
+
+    public override void BackButton()
+    {
+        ChatManager.SkipToEnd = true;
     }
 }
